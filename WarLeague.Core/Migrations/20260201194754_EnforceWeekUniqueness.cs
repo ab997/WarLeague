b4ce17d0eb25e51a63e4306a99186bf4d3783436
Alen Bizjak <alenb@gen-i.si>
2026-02-01 20:48:22 +0100

    force week uniquness

---
+++ WarLeague.Core/Migrations/20260201194754_EnforceWeekUniqueness.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WarLeague.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnforceWeekUniqueness : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Weeks_SeasonId",
+                table: "Weeks",
+                column: "SeasonId",
+                unique: true,
+                filter: "[Status] <> 'Completed' and [Status] <> 'NotOpenYet'");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Weeks_SeasonId",
+                table: "Weeks");
+        }
+    }
+}
